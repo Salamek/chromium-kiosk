68d21e2f415037561402b5c4c753494aa54149ad
Adam Schubert <adam.schubert@sg1-game.net>
2019-10-01 18:26:32 +0200

    New version 0.2.5

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ extra_files = [
 
 setup(
     name='chromium-kiosk',
-    version='0.2.4',
+    version='0.2.5',
     description='Chromium Kiosk',
     long_description=open('README.md').read(),
     author='Adam Schubert',
